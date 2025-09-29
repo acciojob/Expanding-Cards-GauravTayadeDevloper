e0ea2756058213ecfefee8bf6de7c339935b81ad
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-09-29 18:07:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 //your JS code here. If required.
- const panels = document.querySelectorAll('.panel');
+ 
+const panels = document.querySelectorAll('.panel');
+panels.forEach(panel=>{
+	panel.addEventListener('click',()=>{
+		panels.forEach(p=>p.classList.remove('active'));
+		panel.classList.add('active');
+	});
+}); 
 
-    panels.forEach(panel => {
-      panel.addEventListener('click', () => {
-        // Remove active class from all
-        panels.forEach(p => p.classList.remove('active'));
-        // Add active class to clicked
-        panel.classList.add('active');
-      });
-    });
+// panels.forEach(panel => {
+    //   panel.addEventListener('click', () => {
+    //     // Remove active class from all
+    //     panels.forEach(p => p.classList.remove('active'));
+    //     // Add active class to clicked
+    //     panel.classList.add('active');
+    //   });
+    // });
